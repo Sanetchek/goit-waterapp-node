52c84232f3853760e466eac25d9d4e123372db7f
smelik81 <artem.smelyanskiy81@gmail.com>
2024-10-16 17:18:02 +0300

    hange file name

--- src/server.js
+++ src/server.js
@@ -6,9 +6,8 @@ import swaggerDocs from'./middlewares/swaggerDocs.js';
 */
 import { env } from './utils/env.js';
 
-import notFoundHandler from './moddlewares/notFoundHandler.js';
-import errorHandler from './moddlewares/errorHandler.js';
-import UserCollection from './db/models/Users.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const setupServer = () => {
   const app = express();
@@ -21,24 +20,6 @@ const setupServer = () => {
   //app.use('/api-docs', swaggerDocs());
   //app.use('/auth', authRouter);
   //app.use('/water', waterRouter);
-
-  app.get('/auth', async (req, res) => {
-    try {
-      const data = await UserCollection.find();
-      console.log(data);
-      res.json({
-        status: 200,
-        message: 'successfully found users',
-        data,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        status: 500,
-        message: 'Error retrieving users',
-      });
-    }
-  });
 
   app.use('*', notFoundHandler);
   app.use(errorHandler);
