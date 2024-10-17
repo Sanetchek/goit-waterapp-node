1b7be615a7716453ea24c4c16d202ff978ba8ebe
KaterynkaPet <katerinapetrenko85@gmail.com>
2024-10-17 21:53:49 +0300

    add validation

---
+++ src/validation/waters.js
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+export const waterRateSchema = Joi.object({
+    waterRate: Joi.number().min(1).max(15000).required(),     
+});
+
+export const waterNotesSchema = Joi.object({
+    waterVolume: Joi.number().min(1).max(5000).required(),
+    date: Joi.date().iso().required(),
+});
+
+
+
