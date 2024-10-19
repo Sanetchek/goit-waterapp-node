036be57a72e5b36b72e7e94d7363bcb416d278b5
Aleksandr <algryshko@gmail.com>
2024-10-19 11:16:13 +0300

    Update Users.js

--- src/db/models/Users.js
+++ src/db/models/Users.js
@@ -5,7 +5,7 @@ const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: true, // можливо і не обов'язково
+      default: null,
     },
     email: {
       type: String,
@@ -20,7 +20,6 @@ const userSchema = new Schema(
       type: String,
       enum: ['woman', 'man'],
       default: 'woman',
-      required: true,
     },
     weight: {
       type: Number,
