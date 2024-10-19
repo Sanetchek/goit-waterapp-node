d2d52ec83461b3de6ea087fc9b750550b766e49f
Nazar <91900192+Batmann322@users.noreply.github.com>
2024-10-19 10:49:35 +0300

    fix register

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,16 +1,7 @@
 import Joi from 'joi';
 import { emailRegexp } from '../constants/users.js';
 
-export const userRegisterSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+export const userRegisterloginSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(8).required(64),
-  gender: Joi.string().valid('woman', 'man').required(),
-  dailyNormWater: Joi.number().min(500).max(5000).optional(),
-  avatar: Joi.string().optional(),
-});
-
-export const userLoginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(8).required(),
 });
