dd27146470bfbb111091af702c713af6341b0104
smelik81 <artem.smelyanskiy81@gmail.com>
2024-10-21 23:51:23 +0300

    description water-rout

--- src/validation/waters.js
+++ src/validation/waters.js
@@ -1,14 +1,17 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const waterRateSchema = Joi.object({
-    dailyNorm: Joi.number().min(1).max(15000).required(),     
+  dailyNorm: Joi.number().min(1).max(15000).required(),
 });
 
 export const waterNotesSchema = Joi.object({
-    waterVolume: Joi.number().min(1).max(5000).required(),
-    date: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/).required(),
-    dailyNorm: Joi.number().min(1).max(15000).optional(),
+  waterVolume: Joi.number().min(1).max(5000).required(),
+  date: Joi.string()
+    .pattern(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/)
+    .required(),
+  dailyNorm: Joi.number().min(1).max(15000).optional(),
 });
-
-
-
+/* export const waterUpdateNotesSchema = Joi.object({
+  waterVolume: Joi.number().min(1).max(5000).required(),
+  dailyNorm: Joi.number().min(1).max(15000).optional(),
+}); */
